4e244f4b96e10d169afc070f7bdac8fc88a91bf2
rosivaldo junior <91435382+rosivaldocamjr@users.noreply.github.com>
2022-01-19 14:27:48 -0400

    Initial commit

---
+++ src/components/TaskDetails.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import Button from "./Button"
+import { useParams, useHistory } from "react-router-dom"
+import "./TaskDetails.css"
+
+const TaskDetails = () => {
+    const params = useParams()
+    const history = useHistory()
+    const handleBackButtonClick = () => {
+        history.goBack()
+    }
+
+    
+    return (
+        <>
+            <div className="back-button-container">
+                <Button onClick={handleBackButtonClick}>Voltar</Button>
+            </div>
+            <div className="task-details-container">
+                <h2>{params.taskTitle}</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, quia!
+                </p>
+            </div>
+        </>
+    )
+}
+
+export default TaskDetails
